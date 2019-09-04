060ac3d429aa3be90907aac6299eefc4045ccee2
stanislavs <stanislavsojka@seznam.cz>
2019-09-04 23:35:23 +0200

    Handle empty recommendation

--- Widget/Repositories/Implementation/KenticoRecommendedProductsRepository.cs
+++ Widget/Repositories/Implementation/KenticoRecommendedProductsRepository.cs
@@ -9,15 +9,19 @@ using CMS.SiteProvider;
 
 namespace Kentico.Recombee.Repositories.Implementation
 {
+    /// <summary>
+    /// Repository for featured products.
+    /// </summary>
     public class KenticoRecommendedProductsRepository
     {
         /// <summary>
         /// Returns an enumerable collection of products ordered by the date of publication.
+        /// If no recommendation provided, top <paramref name="count"/> products is returned.
         /// </summary>
         /// <param name="filter">Instance of a product filter.</param>
-        /// <param name="count">The number of coffees to return. Use 0 as value to return all records.</param>
-        /// <returns>An enumerable collection that contains the specified number of coffees.</returns>
-        public IEnumerable<SKUTreeNode> GetRecommendedProducts(IList<Guid> recommendations)
+        /// <param name="count">The number of products to return. Use 0 as value to return all records.</param>
+        /// <returns>An enumerable collection that contains the specified number of products.</returns>
+        public IEnumerable<SKUTreeNode> GetRecommendedProducts(IList<Guid> recommendations, int count = 4)
         {
             var recommendationWhere = new WhereCondition();
             if (recommendations.Any())
@@ -25,17 +29,40 @@ namespace Kentico.Recombee.Repositories.Implementation
                 recommendationWhere.WhereIn("DocumentGuid", recommendations);
             }
 
+            AddMissingRecommendations(recommendations, count);
+
             return DocumentHelper.GetDocuments()
                 .Published()
                 .OnSite(SiteContext.CurrentSiteName)
                 .Culture("en-us")
                 .CombineWithDefaultCulture()
-                .TopN(recommendations.Count)
+                .TopN(count)
                 .WhereTrue("SKUEnabled")
                 .Where(recommendationWhere)
                 .OrderByDescending("SKUInStoreFrom")
                 .OfType<SKUTreeNode>()
                 .ToList();
+        }
+
+        private static void AddMissingRecommendations(IList<Guid> recommendations, int count)
+        {
+            if (recommendations.Count < count)
+            {
+                var productGuids = DocumentHelper.GetDocuments()
+                .Published()
+                .OnSite(SiteContext.CurrentSiteName)
+                .Culture("en-us")
+                .CombineWithDefaultCulture()
+                .TopN(count - recommendations.Count)
+                .WhereTrue("SKUEnabled")
+                .WhereNotIn("DocumentGuid", recommendations)
+                .OrderByDescending("SKUInStoreFrom")
+                .Columns("DocumentGuid")
+                .GetListResult<Guid>()
+                .ToList();
+
+                productGuids.ForEach(guid => recommendations.Add(guid));
+            }
         }
     }
 }
