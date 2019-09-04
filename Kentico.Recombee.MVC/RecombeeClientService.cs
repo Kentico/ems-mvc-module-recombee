5fa4bf49741a62382b82f766afb510afd2d0c127
stanislavs <stanislavsojka@seznam.cz>
2019-09-04 23:36:34 +0200

    Catch exceptions from Recombee

--- Kentico.Recombee.MVC/RecombeeClientService.cs
+++ Kentico.Recombee.MVC/RecombeeClientService.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+
+using CMS.Core;
+
 using Kentico.Recombee.Helpers;
+
 using Recombee.ApiClient;
 using Recombee.ApiClient.ApiRequests;
 using Recombee.ApiClient.Bindings;
@@ -23,28 +27,66 @@ namespace Kentico.Recombee
 
         public IList<Guid> RecommendProductsToContact(Guid contactGuid, int count)
         {
-            RecommendationResponse result = client.Send(
-                new RecommendItemsToUser(contactGuid.ToString(), count, cascadeCreate: true, filter: $"('Type' == \"Product\")", logic: new Logic("recombee:personal"))
-            );
-            return result.Recomms.Select(x => Guid.Parse(x.Id)).ToList();
+            var result = default(IList<Guid>);
+            try
+            {
+                var recommendation = client.Send(
+                    new RecommendItemsToUser(contactGuid.ToString(), count, cascadeCreate: true, filter: $"('Type' == \"Product\")", logic: new Logic("recombee:personal"))
+                );
+
+                result = recommendation.Recomms.Select(x => Guid.Parse(x.Id)).ToList();
+            }
+            catch (Exception ex)
+            {
+                result = new List<Guid>();
+                Service.Resolve<IEventLogService>().LogException("Recombee integration module", "RECOMMENDATION", ex);
+
+            }
+
+
+            return result;
         }
 
 
         public void LogPageView(Guid contactGuid, Guid pageGuid)
         {
-            client.Send(new AddDetailView(contactGuid.ToString(), pageGuid.ToString(), cascadeCreate: true));
+            try
+            {
+                client.Send(new AddDetailView(contactGuid.ToString(), pageGuid.ToString(), cascadeCreate: true));
+            }
+            catch (Exception ex)
+            {
+                Service.Resolve<IEventLogService>().LogException("Recombee integration module", "LOGPAGEVIEW", ex);
+            }
+
         }
 
 
         public void LogProductAddition(Guid contactGuid, Guid productGuid, int amount)
         {
-            client.Send(new AddCartAddition(contactGuid.ToString(), productGuid.ToString(), cascadeCreate: true, amount: amount));
+            try
+            {
+                client.Send(new AddCartAddition(contactGuid.ToString(), productGuid.ToString(), cascadeCreate: true, amount: amount));
+            }
+            catch (Exception ex)
+            {
+                Service.Resolve<IEventLogService>().LogException("Recombee integration module", "LOGPRODUCTADDITION", ex);
+            }
+
         }
 
 
         public void LogProductPurchase(Guid contactGuid, Guid productGuid, int amount)
         {
-            client.Send(new AddPurchase(contactGuid.ToString(), productGuid.ToString(), cascadeCreate: true, amount: amount));
+            try
+            {
+                client.Send(new AddPurchase(contactGuid.ToString(), productGuid.ToString(), cascadeCreate: true, amount: amount));
+            }
+            catch (Exception ex)
+            {
+                Service.Resolve<IEventLogService>().LogException("Recombee integration module", "LOGPRODUCTPURCHASE", ex); ;
+            }
+
         }
     }
 }
